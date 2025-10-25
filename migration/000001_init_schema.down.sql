cb0828b7fab1856dc2d2fab7a3954f39480d9e1e
maria <zaush@bk.ru>
2025-10-25 19:24:22 +0300

    BZ-77: обновление миграций

--- migration/000001_init_schema.down.sql
+++ migration/000001_init_schema.down.sql
@@ -2,3 +2,4 @@ DROP TABLE IF EXISTS transactions.requests;
 DROP TABLE IF EXISTS transactions.transactions;
 DROP TYPE IF EXISTS tx_status;
 DROP SCHEMA IF EXISTS transactions;
+DROP EXTENSION IF EXISTS pgcrypto;
